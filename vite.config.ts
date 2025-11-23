61c14f2ba9d755c925404f2d74ca57c340b9b9f2
gramatikov88 <47442563+gramatikov88@users.noreply.github.com>
2025-11-23 16:36:42 +0200

    асфасфас

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
@@ -20,6 +21,8 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
 =======
+=======
+>>>>>>> Stashed changes
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const isBuild = command === 'build';
