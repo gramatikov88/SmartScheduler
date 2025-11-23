7c0a5e6e187d6ab5caa72df8e3bc720954163865
gramatikov88 <47442563+gramatikov88@users.noreply.github.com>
2025-11-23 16:41:48 +0200

    асфасфас

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,6 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-=======
-=======
->>>>>>> Stashed changes
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const isBuild = command === 'build';
@@ -41,7 +20,7 @@ export default defineConfig(({ command, mode }) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
->>>>>>> Stashed changes
       }
-    };
+    }
+  };
 });
